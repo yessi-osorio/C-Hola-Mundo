8c8df9897caf67426c92230eb82bfbf165771e20
yessi-osorio <62948614+yessi-osorio@users.noreply.github.com>
2021-02-23 19:02:16 -0600

    C# Hola Mundo
    Crear una line de texto que diga Hola Mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
